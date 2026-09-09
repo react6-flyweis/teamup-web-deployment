d249d7bc04412bdadd3f40e9ba4824ec11107c1b
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-09-09 15:52:22 +0530

    fix: update king/stud logo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -19,7 +19,7 @@ import SignupSection from './SignupSection';
 
 const logo = '/assets/logo.svg';
 const queenbg = '/assets/queenbg.svg';
-const king = '/assets/Stud.svg';
+const king = '/assets/Stud.png';
 const queen = '/assets/Doe.svg';
 const enochs2 = '/assets/enochs.svg';
 const bits = '/assets/bits.svg';
