8d84d38b61684c4083cd78c065a7f7e6a9664acf
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-09-18 19:52:01 +0530

    update booking path to coming soon

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const BOOKING_PATH = '/coming-soon';
-export const BOOKING_PATH = import.meta.env.VITE_BOOKING_URL || 'https://ecom.roller.app/altitudemanteca/buyapass/';
+export const BOOKING_PATH = '/coming-soon';
+// export const BOOKING_PATH = import.meta.env.VITE_BOOKING_URL || 'https://ecom.roller.app/altitudemanteca/buyapass/';
